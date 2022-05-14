be82f85e1897bd410b2696a3accca2dba64299f7
BaeKwangho <qorhkdgh00@gmail.com>
2022-05-14 19:25:06 +0900

    FEAT: Q&A 기능 추가
    Q&A 등록, 수정, 조회, 관리자 확인까지
    ====
    Member 필드 두개에 대한 FK 설정 확인 필요
    (로직까지 재정의 필요)
    삭제 미구현
    
    배광호

---
+++ src/main/java/com/example/Dokkaebi/service/QnaService.java
@@ -0,0 +1,46 @@
+package com.example.Dokkaebi.service;
+
+import com.example.Dokkaebi.Repository.MemberRepository;
+import com.example.Dokkaebi.Repository.QnaRepository;
+import com.example.Dokkaebi.controller.dtos.QnaReqDto;
+import com.example.Dokkaebi.domain.Member;
+import com.example.Dokkaebi.domain.Qna;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class QnaService {
+    private final QnaRepository qnaRepo;
+    private final MemberRepository memberRepository;
+    public List<Qna> findAll() {return qnaRepo.findAll();}
+    @Transactional
+    public Qna register(QnaReqDto qnaReqDto){
+        Member member = memberRepository.findOne(qnaReqDto.getMemberId());
+        qnaReqDto.setRegiDate(LocalDateTime.now());
+        Qna qna = qnaReqDto.toEntity(member,null);
+        qnaRepo.save(qna);
+        return qna;
+    }
+
+    @Transactional
+    public void reply(QnaReqDto qnaReqDto) throws Exception {
+        Member admin = memberRepository.findOne(qnaReqDto.getAdminId());
+        Qna qna = qnaRepo.findByQnaId(qnaReqDto.getQnaId());
+        qna.RegiRepliedAdmin(admin,qnaReqDto.getComment());
+        qnaRepo.save(qna);
+    }
+
+    @Transactional
+    public void confirm(QnaReqDto qnaReqDto) throws Exception {
+        Qna qna = qnaRepo.findByQnaId(qnaReqDto.getQnaId());
+        qna.AdminConfirm();
+        qnaRepo.save(qna);
+    }
+}
