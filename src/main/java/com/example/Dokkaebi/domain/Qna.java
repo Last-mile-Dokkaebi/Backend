bdfbde88f77d83ab952c716daf64a6b722b90242
BaeKwangho <qorhkdgh00@gmail.com>
2022-05-14 19:25:06 +0900

    FEAT: Q&A 기능 추가
    Q&A 등록, 수정, 조회, 관리자 확인까지
    ====
    Member 필드 두개에 대한 FK 설정 확인 필요
    (로직까지 재정의 필요)
    삭제 미구현
    
    배광호

---
+++ src/main/java/com/example/Dokkaebi/domain/Qna.java
@@ -0,0 +1,52 @@
+package com.example.Dokkaebi.domain;
+
+import com.example.Dokkaebi.common.QnaStatus;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Qna {
+    @Id @GeneratedValue
+    private Long id;
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "member_id")
+    @NotNull
+    private Member member;
+    private Member admin;
+    private LocalDateTime regiDate;
+    private LocalDateTime repliedDate;
+    private String content;
+    private String comment;
+    @Enumerated(EnumType.ORDINAL)
+    private QnaStatus status;
+
+    @Builder
+    public Qna(Member member, Member admin,LocalDateTime repliedDate, LocalDateTime regiDate, String content, String comment, QnaStatus status){
+        this.member=member;
+        this.admin=admin;
+        this.regiDate=regiDate;
+        this.content=content;
+        this.comment=comment;
+        this.status=status;
+        this.repliedDate=repliedDate;
+    }
+    public void RegiRepliedAdmin(Member admin, String comment){
+        this.admin=admin;
+        this.comment=comment;
+        this.status=QnaStatus.RESPONDED;
+        this.repliedDate=LocalDateTime.now();
+    }
+    public void AdminConfirm(){
+        this.status=QnaStatus.UNDERIVIEW;
+    }
+}
